58667272e69acde50b0e4ed9455e59493aedf558
JohnVithor <jv.venceslau.c@gmail.com>
2022-09-24 22:36:36 -0300

    Tratamento inicial de reposta a exceções

---
+++ src/main/java/sloth/basic/error/HTTPErrorResponseBuilder.java
@@ -0,0 +1,16 @@
+package sloth.basic.error;
+
+import sloth.basic.http.HTTPResponse;
+
+public class HTTPErrorResponseBuilder {
+
+    public static HTTPResponse build(int status, RemotingException e) {
+        return new HTTPResponse(
+                "HTTP/1.0",
+                status,
+                HTTPResponse.getMessage(status),
+                HTTPResponse.buildBasicHeaders(e.getMessage()),
+                e.getMessage()
+                );
+    }
+}
---
+++ src/main/java/sloth/basic/error/RemotingException.java
@@ -0,0 +1,8 @@
+package sloth.basic.error;
+
+public class RemotingException extends Exception {
+
+    public RemotingException(String message) {
+        super(message);
+    }
+}
